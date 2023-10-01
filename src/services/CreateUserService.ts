fd44246c782a2bd4d6ea1ad48286533f9abb7ede
aristotelesalves <arystotelys@gmail.com>
2023-10-01 17:40:03 -0300

    correção dos nomes dos arquivos

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -33,7 +33,6 @@ class CreateUserService {
                         {nome},
                         {email}
                     ],
-                    
                 }
             })
             
