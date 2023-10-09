6aa2d9dac3f6550b3bc55af4e38bd6b2c04bfdd1
Caique Gabriel <cgabrieladm@icloud.com>
2023-10-08 21:20:38 -0300

    Ajuste na rota

--- src/Router.ts
+++ src/Router.ts
@@ -1,10 +1,16 @@
 import {Router} from "express"
 import { CreateUserController } from "./controller/CreateUserController"
 import { LoginController } from "./controller/LoginController"
+import { PacientesController } from "./controller/PacientesController"
 
 const router = Router()
 
-router.get('/pacientes')
+router.get('/paciente', new PacientesController().list);
+router.get('/paciente/show', new PacientesController().showOne);
+router.put('/paciente/edit', new PacientesController().edit);
+router.post('/paciente/create', new PacientesController().create);
+router.post('/paciente/active', new PacientesController().active);
+router.post('/paciente/inactive', new PacientesController().inative);
 router.post('/usuario', new CreateUserController().handle)
 router.post('/login', new LoginController().handle)
 
