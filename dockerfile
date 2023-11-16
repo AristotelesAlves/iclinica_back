5d1be3a004d432dc880d9d0a760ab2db573e1cef
aristotelesalves <arystotelys@gmail.com>
2023-11-16 13:24:38 -0300

    docker

--- dockerfile
+++ dockerfile
@@ -3,8 +3,12 @@ FROM node:lts-buster
 WORKDIR /usr/src/app
  
 COPY package*.json ./
+
+COPY prisma/schema.prisma ./
  
 RUN npm install
+
+RUN npx prisma migrate dev
 
 COPY . .
  
--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,20 +1,13 @@
-// Importe as bibliotecas necessárias, incluindo o Prisma
 import { PrismaClient } from '@prisma/client';
 
 // Função para determinar a URL do banco de dados com base na variável de ambiente
-function getDatabaseUrl(): string {
-  const useDatabase1 = process.env.USE_DATABASE_1 === 'true';
+// function getDatabaseUrl(): string {
+//   const useDatabase1 = process.env.USE_DATABASE_1 === 'true';
 
-  return useDatabase1 ? process.env.DATABASE_URL_1 || '' : process.env.DATABASE_URL_2 || '';
-}
+//   return useDatabase1 ? process.env.DATABASE_URL_1 || '' : process.env.DATABASE_URL_2 || '';
+// }
 
-const prismaClient = new PrismaClient({
-    datasources: {
-      db: {
-        url: getDatabaseUrl(),
-      },
-    },
-  });
+const prismaClient = new PrismaClient();
 
 
 export { prismaClient }
